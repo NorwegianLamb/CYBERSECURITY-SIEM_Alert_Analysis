f59665fdd5a22f4ea914ce777cda995248299d8d
NorwegianLamb <norwegianlamb@protonmail.com>
2024-03-15 13:30:12 +0100

    trying to add interactivity to the map

--- src/map_plot.py
+++ src/map_plot.py
@@ -1,41 +1,48 @@
 """
 Author: Flavio Gjoni
 Status: IN-PROGRESS
-Description: With this code we can map the filtered CSV with geopandas and have a visual look at it
+Description: With this code, we can map the filtered CSV with geopandas and have a visual look at it
 """
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import mplcursors
 import sys
 
 def load_data(filepath):
     return pd.read_csv(filepath)
 
-# Map countries to colors based on matching status
 def map_country_colors(df):
     def determine_color(group):
         if (group['CountryMatched'] == 'Match').all():
             return 'match'  # All entries are 'Match'
         else:
             return 'not match'  # At least one entry is 'Not Match' or 'Manual'
-    
     return df.groupby('source.geo.country_name').apply(determine_color).reset_index(name='color')
 
-# Identify unmatched country names between the CSV and the geopandas world dataset
 def get_unmatched_countries(df, world):
     dataset_countries = set(df['source.geo.country_name'].unique())
     world_countries = set(world['name'].unique())
     return dataset_countries - world_countries
 
-def plot_world_map(world, country_colors):
+def hover_function(feature, dataframe):
+    info = feature.get('name')
+    matched_data = dataframe[dataframe['source.geo.country_name'] == info]
+    return matched_data.to_string()
+
+def plot_world_map(world, country_colors, df):
     # fig, ax = plt.subplots(1, figsize=(15, 10))
     ax = world.boundary.plot(edgecolor='black', linewidth=0.2, figsize=(15,10))
-    world.plot(column='color', ax=ax,
-               legend=True, missing_kwds={'color': 'lightgrey'},
+    world.plot(column='color', ax=ax, 
+               legend=True, missing_kwds={'color': 'lightgrey'}, 
                cmap='seismic')
+
+    cursor = mplcursors.cursor(ax, hover=True)
+    cursor.connect("add", lambda sel: sel.annotation.set_text(hover_function(sel.original_target, df)))
+
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    # ax.set_title('Mapped alerts around the world', size=18, weight='bold')
+    
     plt.gcf().canvas.manager.set_window_title('Alert Map')
     plt.show()
 
@@ -49,10 +56,9 @@ def main():
         "The Netherlands": "Netherlands",
         "Hong Kong": "China",  # Hong Kong is part of China in the geopandas dataset
         "Singapore": "Malaysia"  # Singapore // // // Malaysia // // // //
-        # ...
     }
 
-    df = load_data('..\\filtered_alerts_all.csv') # use data\...
+    df = load_data('..\\filtered_alerts_all.csv')
     df = apply_known_country_mappings(df, known_country_mappings)
 
     country_colors = map_country_colors(df)
@@ -64,10 +70,7 @@ def main():
         sys.exit("Address the unmatched countries before proceeding.")
 
     world = world.merge(country_colors, how="left", left_on='name', right_on='source.geo.country_name')
-    plot_world_map(world, country_colors)
-
-    # Save the figure if desired
-    # fig.savefig('data\alerts_mapped.png')
+    plot_world_map(world, country_colors, df)
 
 if __name__ == '__main__':
     main()
