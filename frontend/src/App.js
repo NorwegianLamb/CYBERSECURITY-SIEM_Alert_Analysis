c32c69b2b728705a42fd4e227383e8ced9c69ff1
NorwegianLamb <norwegianlamb@protonmail.com>
2024-04-02 14:33:42 +0200

    input and csv handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.png';
+// import logo from './logo.png';
+import {useState} from 'react';
+import Papa from 'papaparse';
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState([]);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    parseCSV(file);
+  };
+  const parseCSV = (file) => {
+    Papa.parse(file, {
+      complete: (result) => {
+        console.log('Parsed Result:', result);
+        setData(result.data);
+      },
+      header: true,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          Testing React
+          SIEM/SOAR Test
         </p>
+        <input type="file" accept=".csv" onChange={handleFileChange} />
+        <table>
+          <thead>
+            <tr>
+              {data[0] && Object.keys(data[0]).map((header) => (
+                <th key={header}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
